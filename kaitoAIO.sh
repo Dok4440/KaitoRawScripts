4dc612ad4c9cab518d6219d7d928ff6599193f3b
Dok#4440 <75494059+Dok4440@users.noreply.github.com>
2021-03-08 18:15:12 +0100

    Update kaitoAIO.sh

--- kaitoAIO.sh
+++ kaitoAIO.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 echo ""
-echo "Welcome to the Kaito self-host installer. Downloading the latest installer..."
+echo "Fetching & running Kaito_master_runner.sh"
 root=$(pwd)
 
-# ik there's TCU info here from the previous commit (https://github.com/Dok4440/TCUBetaBot/blob/develop/TCURawScripts/linuxAIO.sh)
+wget -N https://raw.githubusercontent.com/Dok4440/KaitoRawScripts/main/Kaito_master_runner.sh
 
-wget -N # link to master_installer
-
-bash Kaito_master_installer.sh
+bash Kaito_master_runner.sh
 cd "$root"
-rm "$root/Kaito_master_installer.sh"
+rm "$root/Kaito_master_runner.sh"
 exit 0
