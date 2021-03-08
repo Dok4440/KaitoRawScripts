82a0be37245e3a65f683c921e56683a135c24585
Dok#4440 <75494059+Dok4440@users.noreply.github.com>
2021-03-08 12:22:59 +0100

    Update kaitoAIO.sh

--- kaitoAIO.sh
+++ kaitoAIO.sh
@@ -4,10 +4,7 @@ echo ""
 echo "Welcome to the Kaito self-host installer. Downloading the latest installer..."
 root=$(pwd)
 
-
-# Starting off; the "linuxAIO.sh" file is the file you always bash whenever changing TCU's config.
-# It simply opens 'TCU_master_installer' but the reason you need to bash THIS file is simple;
-# As you can see at line 36 & 37; it cd's into root and deletes TCU_master_installer after opening the file.
+# ik there's TCU info here from the previous commit (https://github.com/Dok4440/TCUBetaBot/blob/develop/TCURawScripts/linuxAIO.sh)
 
 wget -N # link to master_installer
 
